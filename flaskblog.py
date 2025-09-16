502baabeb8e0f0f02527984dbbfc586718f54b50
Mikayla Solomon <mikaylasolomon68@gmail.com>
2025-09-16 15:22:11 -0400

    trying to figure out extensions

--- flaskblog.py
+++ flaskblog.py
@@ -24,7 +24,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = 'About')
 
 if __name__ == '__main__': # conditional is only true if running script directly
     app.run(debug=True)
